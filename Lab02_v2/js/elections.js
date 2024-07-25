b520e0d273aaf0bb9fb41f802596c19654984537
Kevin Bursaw, EdD <132414922+kmbursaw@users.noreply.github.com>
2024-07-24 21:57:10 -0500

    Add files via upload

--- Lab02_v2/js/elections.js
+++ Lab02_v2/js/elections.js
@@ -4,7 +4,7 @@ window.onload = function() {
     setMap();
 };
 
-//Example 1.3 line 4...set up choropleth map
+//set up map
 function setMap() {
         
     //map frame dimensions
@@ -18,19 +18,19 @@ function setMap() {
         .attr("width", width)
         .attr("height", height);
 
-    //create Albers equal area conic projection centered on United States
+    //create Albers equal area conic projection centered on United States but including Hawaii, Alaska, and PR
     var projection = d3.geoAlbers()
-        .center([-0.00, 42.69])
+        .center([0.00, 48.00])
 
-        .rotate([101.00, -0.00, 0])
+        .rotate([95.55, 0.00, 0])
 
-        .parallels([27.00, 44.52])
+        .parallels([29.50, 45.5])
 
-        .scale(500.0)
+        .scale(427.27)
 
         .translate([width / 2, height / 2]);
 
-
+    //Set path
     var path = d3.geoPath()
         .projection(projection);
     
@@ -39,35 +39,37 @@ function setMap() {
     var promises = [];    
         promises.push(d3.csv("data/popular_vote_by_state.csv")); //load attributes from csv    
         promises.push(d3.json("data/lakes.topojson")); //load background spatial data    
-        promises.push(d3.json("data/states.topojson")); //load choropleth spatial data    
+        promises.push(d3.json("data/US_States_02.TOPOJSON")); //load choropleth spatial data    
         Promise.all(promises).then(callback);
     
-    /*
-    var promises = [
-        d3.csv("data/popular_vote_by_state.csv"),
-        d3.json("data/lakes.topojson"),
-        d3.json("data/states.topojson"),
-    ];
-    Promise.all(promises).then(callback);
-    */
 
-
+    //Error catching to tell user if data not loaded
     Promise.all(promises)
         .then(callback)
         .catch(function(error) {
             console.log("Error loading data:", error);
         });
     
+    //function to call back data and load topojson files and graticules
     function callback(data) {
         var pop_Vote = data[0],
             lakes = data[1],
             states = data[2];
 
-
-        //create graticule generator
+        
+        //create graticule generator, set to every 10 degrees for scale
         var graticule = d3.geoGraticule()
-            .step([5, 5]); //place graticule lines every 5 degrees of longitude and latitude
-
+            .step([10, 10]); //place graticule lines every 5 degrees of longitude and latitude
+        
+        /*   
+        //create graticule background
+        var gratBackground = map.append("path")
+            .datum(graticule.outline()) //bind graticule background
+            .attr("class", "gratBackground") //assign class for styling
+            .attr("d", path) //project graticule
+        */
+            
+        
         //create graticule lines
         var gratLines = map.selectAll(".gratLines") //select graticule elements that will be created
             .data(graticule.lines()) //bind graticule lines to each element to be created
@@ -75,25 +77,13 @@ function setMap() {
             .append("path") //append each element to the svg as a path element
             .attr("class", "gratLines") //assign class for styling
             .attr("d", path); //project graticule lines
+        
+        
 
-
-        //Example 2.6 line 5...create graticule lines
-        var gratLines = map.selectAll(".gratLines") //select graticule elements that will be created
-            .data(graticule.lines()) //bind graticule lines to each element to be created
-            .enter() //create an element for each datum
-            .append("path") //append each element to the svg as a path element
-            .attr("class", "gratLines") //assign class for styling
-            .attr("d", path); //project graticule lines
-
-
-
-        console.log("pop vote data:",pop_Vote);
-        console.log("lakes data:", lakes);
-        console.log("states data:", states);
 
         //translate  TopoJSON
         var lakesFeature = topojson.feature(lakes, lakes.objects.great_lakes_01),
-            statesFeature = topojson.feature(states, states.objects.US_States_01).features;
+            statesFeature = topojson.feature(states, states.objects.US_States_02).features;
 
 
         
@@ -111,12 +101,6 @@ function setMap() {
             .attr("class", "states")
             .attr("d", path);
     
-
-
-
-        //examine the results
-        console.log(lakesPath);
-        console.log(statesPath);
     };
 };
 
